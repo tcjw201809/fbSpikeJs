4e54783f5ec82f2f27b9c1038b8cca413e047dc9
千葉 健司 <t_chiba@joyworks.jp>
2020-07-29 19:00:40 +0900

    初期実装:Webでは動作

--- App.js
+++ App.js
@@ -1,16 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import LoginForm from './src/LoginForm';
+//import Header from './src/Header';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyBQV2JTF_qKU-7Kd58i8GQHbwcnFJ6r9BI",
+      authDomain: "sample1-ae04d.firebaseapp.com",
+      databaseURL: "https://sample1-ae04d.firebaseio.com",
+      projectId: "sample1-ae04d",
+      storageBucket: "sample1-ae04d.appspot.com",
+      messagingSenderId: "588935163359",
+      appId: "1:588935163359:web:868c203cc4627725c6686a",
+      measurementId: "G-D96R7X17JK"
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true});
+      } else {
+        this.setState({ loggedIn: false});
+      }
+    });
+  }
+  
+  render() {
+    return (
+      <View>
+        <LoginForm />
+      </View>
+    )
+  }
 }
 
+export default App;
+/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,3 +47,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
